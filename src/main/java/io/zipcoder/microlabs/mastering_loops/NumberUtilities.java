2a517689096d1993d5a1f77a9a87b50d3d29d701
Liz <liz@Zipcoders-MacBook-Pro-10.local>
2023-06-29 22:59:58 -0400

    Nailed it

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,67 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNumbers = "";
+        for (int i = start; i < stop; i ++){
+            if ( i % 2 == 0 ) {
+                evenNumbers += i;
+            }
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNumbers = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 ==1 ) {
+                oddNumbers += i;
+            }
+        }
+        return oddNumbers;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squareNumbers = "";
+        for (int i = start; i < stop; i += step) {
+            squareNumbers += (i*i);
+        }
+        return squareNumbers;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String range = "";
+        for (int i = 0; i < stop; i++){
+            range += i;
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++) {
+            range += i;
+        }
+        return range;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i += step){
+            range += i;
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder builder = new StringBuilder("");
+
+        for (int i = start; i < stop; i += step) {
+            builder.append((int)Math.pow(i,exponent));
+        }
+        return builder.toString();
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,32 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for (int i = 1; i <= numberOfRows; i ++) {
+            for (int j = 1; j <= i; j++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        for (int i = 1; i <= numberOfStars; i++) {
+            row += "*";
+        }
+
+        return row;
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        return getTriangle(9);
     }
 }
